3bbdcc362089c580edff2a044984e029880a5946
BharatSharma29 <bharat429sharma@gmail.com>
2026-04-08 15:39:35 +0100

    routes added

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,20 +1,20 @@
-// Add evidence routes
-
 import express from "express";
 import cors from "cors";
+
 import controlRoutes from "./routes/controlRoutes.js";
 import evidenceRoutes from "./routes/evidenceRoutes.js";
 
 const app = express();
 
-app.use(cors({ origin: "*" }));
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Compliance API running on EC2 🚀");
 });
 
+// Routes
 app.use("/api/controls", controlRoutes);
-app.use("/api/evidence", evidenceRoutes); // NEW
+app.use("/api/evidence", evidenceRoutes); // 👈 MUST EXIST
 
 export default app;
